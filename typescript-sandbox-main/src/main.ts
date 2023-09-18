029f84d608a1f6bc8081482d0a88059a6388bda7
JoseManuel <josemanuelescolano1@gmail.com>
2023-09-18 14:20:13 +0200

    ENTREGA JAVASCRIPT 1

--- typescript-sandbox-main/src/main.ts
+++ typescript-sandbox-main/src/main.ts
@@ -1,3 +1,10 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
+console.log("Laboratorio Módulo 2");
+
+//Ticket de 120 euros para 6 personas
+console.log(120 - 18); //descontamos del total el valor de las bebidas
+console.log(102 / 6); //el resultado anterior lo dividimos entre 6
+
+//Para hacerlo todo junto
+console.log((120 - 18) / 6);
