61930313f1c2af95cc07df810965a94f2311cbc7
JoseManuel <josemanuelescolano1@gmail.com>
2023-09-23 11:47:46 +0200

    PRÁCTICCA VARIABLES Y TIPOS

---
+++ typescript-sandbox-main/src/main copy.ts
@@ -0,0 +1,25 @@
+import "./style.css";
+console.log("Laboratorio Módulo 2");
+
+//Variables: es cómo una cajita mágica en la que podemos guardar
+//información para usarla más tarde dándoles un nombre para
+//encontrarlas más fácil
+
+/*
+- Título: 'The Catcher in the Rye'
+- Autor: 'J.D. Salinger'
+- Año de publicación: 1951
+*/
+
+let libro = {
+    titulo: "The Catcher in the Rye",
+    autor: "J.D. Salinger",
+    añoPublicacion: 1951,
+};
+
+libro = {
+    titulo: "Fundación",
+};
+
+libro.añoPublicacion = 1952;
+
